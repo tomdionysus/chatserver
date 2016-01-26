a3a0c02ad714d8832042fd391851894ee8d69fe8
James Stevenson <james@stev.org>
2016-01-26 13:52:30 +0000

    Adds error handling

--- src/chatserver.c
+++ src/chatserver.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -13,25 +15,53 @@ int main() {
 	
 	printf("Chatserver Starting on %d\n",CHATSERVER_PORT);
 
-	bzero( &server_addr, sizeof(server_addr));
+	memset( &server_addr, 0, sizeof(server_addr));
 
 	listen_sock_id = socket(AF_INET,SOCK_STREAM,0);
+	if (listen_sock_id < 0)
+	{
+		perror("socket:");
+		exit(EXIT_FAILURE);
+	}
 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(CHATSERVER_PORT);
 
-	bind(listen_sock_id, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if (bind(listen_sock_id, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+	{
+		perror("bind:");
+		exit(EXIT_FAILURE);
+	}
 	
-	listen(listen_sock_id, 1);
+	if (listen(listen_sock_id, 1) < 0)
+	{
+		perror("listen:");
+		exit(EXIT_FAILURE);
+	}
 
 	printf("Chatserver Listening\n");
 
 	coms_sock_id = accept(listen_sock_id, NULL, NULL);
-	
-	char str[] = "Hello :)\n\x00";
-	write(coms_sock_id, str, strlen(str));
+	if (coms_sock_id < 0)
+	{
+		fprintf(stderr, "accept failed errno: %d msg: %s\n", errno, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
-	close(listen_sock_id);
+	char str[] = "Hello :)\n\x00";
+	if (write(coms_sock_id, str, strlen(str)) < 0)
+	{
+		perror("write:");
+		exit(EXIT_FAILURE);
+	}
+
+	if (close(listen_sock_id) < 0)
+	{
+		perror("close:");
+		exit(EXIT_FAILURE);
+	}
+
+	return EXIT_SUCCESS;
 }
 
